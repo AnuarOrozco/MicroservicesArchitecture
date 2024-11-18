6809e296a98d0458694b419a4674314923c1c6e7
Anuar Orozco <anuarorozzco@gmail.com>
2024-11-18 17:00:32 -0600

    Fixed some errors that wont let course and student start up

--- microservice-student/microservice-student/src/main/java/com/microservice/student/persistence/IStudentRepository.java
+++ microservice-student/microservice-student/src/main/java/com/microservice/student/persistence/IStudentRepository.java
@@ -3,6 +3,7 @@ package com.microservice.student.persistence;
 import com.microservice.student.entities.Student;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,8 +11,8 @@ import java.util.List;
 @Repository
 public interface IStudentRepository extends CrudRepository<Student, Long> {
 
-    @Query("SELECT s FROM Student WHERE s.courseId = :idCourse")
-    List<Student> findAllStudents(Long idCourse);
+    @Query("SELECT s FROM Student s WHERE s.courseId = :idCourse")
+    List<Student> findAllStudents(@Param("idCourse") Long idCourse);
 
     //List<Student> findAllByCourseId(Long idCourse);
 
